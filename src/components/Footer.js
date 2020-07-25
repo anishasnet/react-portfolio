d135ccdf9d21f68f09695f38114f4740cd417ec2
anishasnet <anishasnet@gmail.com>
2020-07-25 15:14:47 -0700

    Footer created + images added

---
+++ src/components/Footer.js
@@ -0,0 +1,18 @@
+import React from "react";
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+const Footer = () => (
+    <>
+  <div className="footer">
+    <a href="https://github.com/anishasnet" target="_blank" rel="noopener noreferrer">
+        <img alt="GitHub" className="footer-img" src={require("../assets/icons/github.png")} />
+    </a> 
+
+    <a href="https://www.linkedin.com/in/izabela-petrovicova-320b1620/" target="_blank" rel="noopener noreferrer">
+        <img alt="LinkedIn" className="footer-img" src={require("../assets/icons/linkedin.png")} />
+    </a> 
+  </div>
+  </>
+);
+
+export default Footer;
