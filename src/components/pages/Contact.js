d6c3d2e995f013e286ee9e21eb9644c403efa9db
anishasnet <anishasnet@gmail.com>
2020-07-26 12:06:28 -0700

    add react and validate email imports in contact

--- src/components/pages/Contact.js
+++ src/components/pages/Contact.js
@@ -1,3 +1,6 @@
+import React, { useState } from "react";
+import { validateEmail} from '../../utils/helpers';
+
 function Contact() {
     const [formState, setFormState] = useState({name: '', email: '', message: '',});
     const { name, email, message } = formState
