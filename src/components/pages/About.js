c7f5ce41a337ae39e39adfeb9d56055a63387edc
anishasnet <anishasnet@gmail.com>
2020-07-26 11:34:40 -0700

    Created about me javascript and added images to assets

---
+++ src/components/pages/About.js
@@ -0,0 +1,27 @@
+import React from "react";
+import coverImage from '../../assets/images/coverImage.jpg';
+import profilePicture from '../../assets/images/profilePicture.jpg';
+
+function About() {
+    return (
+        <section className="">
+            <h1 id="about">Anisha Sinha</h1>
+            <img src={coverImage} className="hero"  alt="cover" />
+
+            <div className="row">
+                <div className="col-4 profile-picture">
+                    <img src={profilePicture} className="profile-picture" alt="anishasnet" />
+                </div>
+
+                <div className="col-7 bio">
+                    <h4>About me</h4>
+                    <p>
+                    Anisha Sinha is a student at Purdue University studing Computer Science pursuing a B.S. She is expected to graduate in 2023. 
+                    She graduated Monta Vista High School in Cupertino, CA in 2019. In her free time, Anisha likes hiking and spending time with friends. 
+                    </p>
+                </div>
+            </div>
+        </section>
+    )
+}
+export default About;
