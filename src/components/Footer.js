b81c6dbdd23a7c990e1cc4cc218691fb220e29e7
anishasnet <anishasnet@gmail.com>
2020-07-26 12:13:28 -0700

    Added stack overflow to footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -7,8 +7,12 @@ const Footer = () => (
         <img alt="GitHub" className="footer-img" src={require("../assets/icons/github.png")} />
     </a> 
 
-    <a href="https://www.linkedin.com/in/izabela-petrovicova-320b1620/" target="_blank" rel="noopener noreferrer">
+    <a href="https://www.linkedin.com/in/anisha-sinha-057170165/" target="_blank" rel="noopener noreferrer">
         <img alt="LinkedIn" className="footer-img" src={require("../assets/icons/linkedin.png")} />
+    </a> 
+
+    <a href="https://stackoverflow.com/users/12996399/anishasnet" target="_blank" rel="noopener noreferrer">
+        <img alt="StackOverflow" className="footer-img" src={require("../assets/icons/stackoverflow.png")} />
     </a> 
   </div>
   </>
