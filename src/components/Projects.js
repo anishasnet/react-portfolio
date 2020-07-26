05d5f261a25b643b544475f95152a030c11f353b
anishasnet <anishasnet@gmail.com>
2020-07-26 14:22:16 -0700

    Added list of projects

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,14 +1,14 @@
 import React, { useState} from "react";
-import projects from '../projects.json';
+import projects from '../project-list.json';
 
 function ProjectCard(props) {
   return (
     <div className="card-container col-12">
         <div className="card">
-          <img alt={props.name} src={props.image} className="card-img" />
+          <img alt={props.proj_name} src={props.image} className="card-img" />
           <h3>{props.name}</h3>
           <div>
-              <a href={props.deployedapp} target="_blank" rel="noopener noreferrer">Deployed Application</a>  
+              <a href={props.deployed_app} target="_blank" rel="noopener noreferrer">Deployed Application</a>  
           </div>
           <a href={props.github} target="_blank" rel="noopener noreferrer">GitHub</a>
       </div>  
@@ -21,15 +21,15 @@ function Wrapper(props) {
 }
 
 function Projects() {
-    const [projectsList, setProjectsList] = useState(projects);
+    const [projectsList] = useState(projects);
     const renderProject = (project) => {
         return (
             <ProjectCard 
-            name={project.name} 
+            proj_name={project.proj_name} 
             image={project.image} 
             id={project.id} 
+            deployed_app={project.deployed_app}
             github={project.github} 
-            deployedapp={project.deployedapp}
             key={project.id}
             />
         )
