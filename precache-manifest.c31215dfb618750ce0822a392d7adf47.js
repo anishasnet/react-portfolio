00143d032d98bf45288749f219ef084b244e6cfc
anishasnet <anishasnet@gmail.com>
2020-07-26 23:25:43 -0700

    Updates

---
+++ precache-manifest.c31215dfb618750ce0822a392d7adf47.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0e52d31af092e1732045a312d5a2510e",
+    "url": "/react-portfolio/index.html"
+  },
+  {
+    "revision": "55509b72dc72f5ecc2ce",
+    "url": "/react-portfolio/static/css/main.f66032c5.chunk.css"
+  },
+  {
+    "revision": "0cc899523288cbd71a44",
+    "url": "/react-portfolio/static/js/2.afc97f29.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-portfolio/static/js/2.afc97f29.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "55509b72dc72f5ecc2ce",
+    "url": "/react-portfolio/static/js/main.d727fbab.chunk.js"
+  },
+  {
+    "revision": "aff33cef9dbb8787d6b3",
+    "url": "/react-portfolio/static/js/runtime-main.213b1880.js"
+  },
+  {
+    "revision": "018916a3a9bb55b83e6c3ed1a12c8f8d",
+    "url": "/react-portfolio/static/media/coverImage.018916a3.jpg"
+  },
+  {
+    "revision": "09f5c6ae0d73877c4fc2595170735ff9",
+    "url": "/react-portfolio/static/media/github.09f5c6ae.png"
+  },
+  {
+    "revision": "2abdf5d0354d31da56375efb10662613",
+    "url": "/react-portfolio/static/media/linkedin.2abdf5d0.png"
+  },
+  {
+    "revision": "592c287f4b7019141aa6ff172eab32e9",
+    "url": "/react-portfolio/static/media/profilePicture.592c287f.jpg"
+  }
+]);
